152f214aec7e7826fa32c6e7314943c3e80b685b
Jeremiah Orem <oremj@oremj.com>
2013-09-14 20:34:10 -0700

    Add connection limiter.

--- pushtestpod/pushtestpod.go
+++ pushtestpod/pushtestpod.go
@@ -3,7 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
-	"sync"
+	"log"
 	"time"
 )
 
@@ -11,6 +11,7 @@ var server = flag.String("server", "localhost", "Pushgo Server Name")
 var secure = flag.Bool("secure", false, "Use wss/https")
 var numClients = flag.Int("clients", 1, "Number of concurrent clients")
 var delay = flag.Int("delay", 10000, "Delay between PUTs in milliseconds.")
+var connectionLimit = flag.Int("connectlimit", 100, "Connection limiter.")
 
 func incStat(name string) {
 	counterChan <- &stat{name, 1}
@@ -22,12 +23,14 @@ type stat struct {
 }
 
 var counterChan chan *stat
-var waitConnect chan bool
 
 func main() {
 	flag.Parse()
 	counterChan = make(chan *stat, 100000)
-	waitConnect = make(chan bool, *numClients)
+	port := 80
+	if *secure {
+		port = 443
+	}
 
 	metrics := make(Metrics)
 	go func() {
@@ -36,47 +39,59 @@ func main() {
 		}
 	}()
 
-	port := 80
-	if *secure {
-		port = 443
-	}
-
 	go func() {
-		clients := make([]*Client, 0, *numClients)
-		var wg sync.WaitGroup
+		waitHandshake := make(chan bool)
+		waitRegister := make(chan bool)
+		connectChan := make(chan bool, *numClients)
+		handshakeChan := make(chan bool)
+		connectLimiter := make(chan bool, *connectionLimit)
 		for i := 0; i < *numClients; i++ {
 			c := NewClient(*server, port, &Config{secure: *secure, delay: *delay})
-			clients = append(clients, c)
-			wg.Add(1)
+			connectLimiter <- true
 			go func(c *Client) {
-				defer wg.Done()
+				firstConnect := true
+				firstHandshake := true
 				for {
 					err := c.Connect()
 					if err != nil {
 						time.Sleep(2 * time.Second)
+					}
+
+					if firstConnect {
+						<-connectLimiter
+						connectChan <- true
+						firstConnect = false
+					}
+
+					<-waitHandshake
+					log.Println("Handshaking")
+					err = c.Handshake()
+					if err != nil {
+						time.Sleep(2 * time.Second)
 						continue
 					}
-					return
+					if firstHandshake {
+						handshakeChan <- true
+						firstHandshake = true
+					}
+
+					<-waitRegister
+					log.Println("Registering")
+					c.Register()
+					c.Run()
 				}
 			}(c)
 		}
-		wg.Wait()
 
-		for _, c := range clients {
-			wg.Add(1)
-			go func(c *Client) {
-				defer wg.Done()
-				c.Handshake()
-			}(c)
+		for i := 0; i < *numClients; i++ {
+			<-connectChan
 		}
-		wg.Wait()
+		close(waitHandshake)
 
-		for _, c := range clients {
-			go func(c *Client) {
-				c.Register()
-				c.Run()
-			}(c)
+		for i := 0; i < *numClients; i++ {
+			<-handshakeChan
 		}
+		close(waitRegister)
 	}()
 
 	var tmp Metrics
